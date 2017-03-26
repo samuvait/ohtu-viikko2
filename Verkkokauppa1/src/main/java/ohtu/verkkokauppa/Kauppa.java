3b56151e647fc7fb551720842195425c4a9daee1
samuvait <samu.vaittinen@helsinki.fi>
2017-03-26 17:35:24 +0300

    tehtava 8

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -2,16 +2,16 @@ package ohtu.verkkokauppa;
 
 public class Kauppa {
 
-    private Varasto varasto;
-    private Pankki pankki;
+    private VarastoIO varasto;
+    private PankkiIO pankki;
     private Ostoskori ostoskori;
-    private Viitegeneraattori viitegeneraattori;
+    private ViitegeneraattoriIO viitegeneraattori;
     private String kaupanTili;
 
-    public Kauppa() {
-        varasto = Varasto.getInstance();
-        pankki = Pankki.getInstance();
-        viitegeneraattori = Viitegeneraattori.getInstance();
+    public Kauppa(VarastoIO varasto, PankkiIO pankki, ViitegeneraattoriIO viitegeneraattori) {
+        this.varasto = varasto;
+        this.pankki = pankki;
+        this.viitegeneraattori = viitegeneraattori;
         kaupanTili = "33333-44455";
     }
 
